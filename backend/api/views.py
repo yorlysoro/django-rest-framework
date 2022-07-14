a3c2df19df3a3503da4ca1f132e83ac52e478995
yorlysoro <yorlysoro@gmail.com>
2022-07-13 22:06:05 -0400

    gettin started in drf

---
+++ backend/api/views.py
@@ -0,0 +1,17 @@
+import json
+from django.forms.models import model_to_dict
+#from django.http import JsonResponse, HttpResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+@api_view(["POST"])
+def api_home(request, *args, **kwargs):
+	serializer = ProductSerializer(data=request.data)
+	if serializer.is_valid(raise_exception=True):
+		print(serializer.data)
+		return Response(serializer.data)
+	return Response({"invalid" : "not good data"}, status=400)
