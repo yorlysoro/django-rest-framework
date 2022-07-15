bdc431181d120570791d49d0b9de028a9f0c7fe1
yorlysoro <yorlysoro@gmail.com>
2022-07-14 22:06:33 -0400

    new views for drf

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,8 +3,28 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ProductSerializer
+
+	def perform_create(self, serializer):
+		title = serializer.validated_data.get('title')
+		content = serializer.validated_data.get('content')
+		if content is None:
+			content = title
+		serializer.save(content=content)
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
 	queryset = Product.objects.all()
 	serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ProductSerializer
+
+product_list_view = ProductListAPIView.as_view()
